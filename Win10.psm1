c2214d290b8f73da10eaee63f1e5d7fdc37dd4a3
mistermert504 <58525648+mistermert504@users.noreply.github.com>
2019-12-04 19:26:53 +0100

    Update Win10.psm1

--- Win10.psm1
+++ Win10.psm1
@@ -3675,6 +3675,56 @@ Function DeleteShortcutEdge {
 }
 
 
+Function InstallBasicSoftware {
+	Write-Output "Install Basic Software..."
+	#   Description:
+	# This script will use Windows package manager to bootstrap Chocolatey and
+	# install a list of packages. Script will also install Sysinternals Utilities
+	# into your default drive's root directory.
+	
+	$packages = @(
+    "vscode"
+    "epicgameslauncher"
+    "steam"
+    "winscp"
+    "dashlane"
+    "googlechrome"
+    "putty"
+    "qbittorrent"
+    "speedcrunch"
+    "wireshark"
+		"spotify"
+		"geforce-game-ready-driver"
+		"rainmeter"
+		"gimp"
+		"python3"
+		"paint.net"
+		"innosetup"
+	)
+	Write-Output "Setting up Chocolatey software package manager"
+	Get-PackageProvider -Name chocolatey -Force
+
+	echo "Setting up Full Chocolatey Install"
+	Install-Package -Name Chocolatey -Force -ProviderName chocolatey
+	$chocopath = (Get-Package chocolatey | ?{$_.Name -eq "chocolatey"} | Select @{N="Source";E={((($a=($_.Source -split "\\"))[0..($a.length - 2)]) -join "\"),"Tools\chocolateyInstall" -join "\"}} | Select -ExpandProperty Source)
+	& $chocopath "upgrade all -y"
+	choco install chocolatey-core.extension --force
+
+	echo "Creating daily task to automatically upgrade Chocolatey packages"
+	# adapted from https://blogs.technet.microsoft.com/heyscriptingguy/2013/11/23/using-scheduled-tasks-and-scheduled-jobs-in-powershell/
+	$ScheduledJob = @{
+			Name = "Chocolatey Daily Upgrade"
+			ScriptBlock = {choco upgrade all -y}
+			Trigger = New-JobTrigger -Daily -at 2am
+			ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+	}
+	Register-ScheduledJob @ScheduledJob
+
+	echo "Installing Packages"
+	$packages | %{choco install $_ --force -y}
+
+}
+
 ##########
 #endregion Auxiliary Functions
 ##########
